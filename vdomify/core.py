407876e18779f1f53740dd908959d51c171dde7d
Kyle Kelley <rgbkrk@gmail.com>
2017-10-08 13:24:45 -0700

    convert even more types, including vdom itself

--- vdomify/core.py
+++ vdomify/core.py
@@ -1,17 +1,20 @@
 from IPython.core.formatters import DisplayFormatter
 
-from .transforms import Plain
+from .transforms import Plain, HTML, VDOM
 
 _default_display_order = [
     'application/vdom.v1+json',
     # TODO: Bring in minidom parsing logic
-    # 'text/html',
+    'text/html',
     'text/plain'
 ]
 
 _default_transforms = {
-    'text/plain': Plain
+    'text/plain': Plain,
+    'text/html': HTML,
+    'application/vdom.v1+json': VDOM
 }
+
 
 def _acquire_formatter():
     try:
@@ -22,29 +25,31 @@ def _acquire_formatter():
         # otherwise use the default
         return DisplayFormatter()
 
-def _get_richest(obj, formatter, display_order):
+
+def _get_richest(obj, formatter, display_order, transforms):
     (data, metadata) = formatter.format(obj)
     bundle = data
 
     supported_types = set(data.keys()).intersection(display_order)
 
     for mimetype in display_order:
-        if mimetype in supported_types:
-            yield { "type": mimetype, "payload": bundle[mimetype] }
-            return
+        if mimetype in supported_types and mimetype in transforms:
+            yield transforms[mimetype](bundle[mimetype])
 
-    # should not get here, we'll do a default 'text/plain' using repr for now...
-    yield { "type": 'text/plain', "payload": repr(obj)}
+    # We shouldn't get here, however vdom allows `null` entries, so we'll return
+    # None explicitly
+    return None
+
 
 def vdomify(o, display_order=None, transforms=None):
     formatter = _acquire_formatter()
 
-    if(display_order is None):
+    if (display_order is None):
         display_order = _default_display_order
 
-    if(transforms is None):
+    if (transforms is None):
         transforms = _default_transforms
 
-    richest = next(_get_richest(o, formatter, display_order=display_order))
-
-    return transforms[richest['type']](richest['payload'])
+    return next(
+        _get_richest(
+            o, formatter, display_order=display_order, transforms=transforms))
--- vdomify/transforms.py
+++ vdomify/transforms.py
@@ -1,4 +1,14 @@
 from vdom.helpers import div, pre
 
+
 def Plain(data):
     return pre(data)
+
+
+def HTML(data):
+    # TODO: use xml.dom.minidom as dangerouslySetInnerHTML should be blocklisted in frontends
+    return div(dangerouslySetInnerHTML={'__html': data})
+
+def VDOM(data):
+    # noop
+    return data
